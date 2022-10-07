fb27dbd7a0666f04a9b54339b0f8545660d0d4ff
Elroy Kanye <elroykanye@gmail.com>
2022-10-07 21:20:17 +0100

    add student-class-level service test

---
+++ src/app/services/student-class-level.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StudentClassLevelService } from './student-class-level.service';
+
+describe('StudentClassLevelService', () => {
+  let service: StudentClassLevelService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StudentClassLevelService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
