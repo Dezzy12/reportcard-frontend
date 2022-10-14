a66d28fabbf57278d3d3529a831e2b9cf40e21cb
Elroy Kanye <elroykanye@gmail.com>
2022-10-14 21:40:22 +0100

    complete logic for creating student application in student home

--- src/app/modules/rc-student/components/student-home/student-home.component.ts
+++ src/app/modules/rc-student/components/student-home/student-home.component.ts
@@ -1,6 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../../services/user.service";
 import {UserComplete} from "../../../../models/dto/user.model";
+import {StudentService} from "../../../../services/student.service";
+import {StudentApplicationService} from "../../../../services/student-application.service";
+import {ApplicationRequest, StudentApplicationTrial} from "../../../../models/dto/student-application.model";
+import {Student} from "../../../../models/dto/student.model";
+import {RcClassLevel, StudentClassLevel} from "../../../../app.types";
+import {StudentClassLevelService} from "../../../../services/student-class-level.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-student-home',
@@ -9,17 +16,58 @@ import {UserComplete} from "../../../../models/dto/user.model";
 })
 export class StudentHomeComponent implements OnInit {
   student?: UserComplete;
+  sat?: StudentApplicationTrial;
+  studentClassLevels: StudentClassLevel[] = [];
+  showApplyDialog: boolean = false;
+  selectClassLevelForm: FormGroup ;
 
   constructor(
+    private _fb: FormBuilder,
     private _userService: UserService,
+    private _studentService: StudentService,
+    private _studentClassLevelService: StudentClassLevelService,
+    private _studentApplicationService: StudentApplicationService,
   ) {
+    this.selectClassLevelForm = this._fb.group({
+      classLevel: [0, Validators.required]
+    })
   }
 
   ngOnInit(): void {
     this.loadUserComplete();
   }
 
-  loadUserComplete = () => this._userService.getCompleteFromSession().subscribe(u => this.student = u);
+  loadUserComplete = () => this._userService.getCompleteFromSession().subscribe(u => {
+    this.student = u;
+    this.loadClassLevels((u.account as Student).schoolId);
+    this.loadCurrentStudentApplicationTrial((u.account as Student).id);
+  });
 
+  loadClassLevels = (schoolId: number) => this._studentClassLevelService.getAllBySchool(schoolId).subscribe((res) => {
+    console.log(res)
+    this.studentClassLevels = res;
+  })
 
+  loadCurrentStudentApplicationTrial = (studentId: number) => {
+    this._studentApplicationService.getTrialByStudentAndCurrentYear(studentId).subscribe(trial => {
+      if (trial) {
+        this.showApplyDialog = false;
+        this.sat = trial;
+      } else {
+        this.showApplyDialog = true;
+      }
+    });
+  }
+
+  createStudentApplicationAction = () => {
+    if (this.student) {
+      const classSubId: number = this.selectClassLevelForm.get('classLevel')?.value
+      const applicationRequest: ApplicationRequest = {
+        studentId: (this.student.account as Student).id,
+        classSubId: classSubId,
+        yearId: -1 // setting this so the current academic year is used instead
+      }
+      this._studentApplicationService.save(applicationRequest).subscribe();
+    }
+  }
 }
