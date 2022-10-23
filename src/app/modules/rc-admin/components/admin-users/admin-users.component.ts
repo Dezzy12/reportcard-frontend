427021dd87cae6529f9de347a5ca8e5de137ed58
Elroy Kanye <elroykanye@gmail.com>
2022-10-23 13:10:02 +0100

    load users by roles

--- src/app/modules/rc-admin/components/admin-users/admin-users.component.ts
+++ src/app/modules/rc-admin/components/admin-users/admin-users.component.ts
@@ -9,8 +9,15 @@ import {UserService} from "../../../../services/user.service";
 })
 export class AdminUsersComponent implements OnInit {
   users: User[] = [];
+  selectedRoleId: number = -1;
+  roles: {id: number, name: string}[] = [];
 
   constructor(private _userService: UserService) {
+    this.roles = [
+      {id: 1, name: 'Admin'},
+      {id: 2, name: 'Student'},
+      {id: 3, name: 'Teacher'}
+    ];
   }
 
   ngOnInit(): void {
@@ -20,4 +27,17 @@ export class AdminUsersComponent implements OnInit {
   loadUsers = () => this._userService.getAll().subscribe(users => this.users = users);
 
   toggleApprovedAction = (id: number) => this._userService.toggleApproved(id).subscribe();
+
+  onSelectedRoleAction() {
+    switch (this.selectedRoleId) {
+      case 1:
+        this._userService.getAllAdmin().subscribe(users => this.users = users); break;
+      case 2:
+        this._userService.getAllStudent().subscribe(users => this.users = users); break;
+      case 3:
+        this._userService.getAllTeacher().subscribe(users => this.users = users); break;
+      default:
+        this.loadUsers();
+    }
+  }
 }
